580c7291ff2f7a4e955f4d01ffc1be40d1dfd01f
Fernando Gallegos <gallegosfernando0043@gmail.com>
2025-08-31 02:56:30 -0600

    Primer_dia

---
+++ I_O_de_datos_JoptionPane.java
@@ -0,0 +1,24 @@
+package CURSO;
+
+import javax.swing.JOptionPane;
+
+public class I_O_de_datos_JoptionPane {
+    public static void main(String[] args) {
+        
+        String cadena;
+        int entero;
+        double decimal;
+        char caracter;
+
+        cadena = JOptionPane.showInputDialog("Digite una cadena: ");
+        entero = Integer.parseInt(JOptionPane.showInputDialog("Digite un entero: "));
+        caracter = JOptionPane.showInputDialog("Digite un caracter: ").charAt(0);
+        decimal = Double.parseDouble(JOptionPane.showInputDialog("DIgite un decimal: "));
+
+        JOptionPane.showMessageDialog(null,"La cadena es: " + cadena);
+        JOptionPane.showMessageDialog(null," El entero es: " + entero);
+        JOptionPane.showMessageDialog(null, "El caracter es: "+ caracter);
+        JOptionPane.showMessageDialog(null, "El decimal es: " + decimal);
+        
+    }
+}
