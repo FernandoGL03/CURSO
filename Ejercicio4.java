49a3cd593eff71ed719f6ecf13014e56b363e1f6
Fernando Gallegos <gallegosfernando0043@gmail.com>
2025-09-02 02:10:30 -0600

    Ejercicio5

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,3 +1,10 @@
+/*La calificacion final  de un estudiante de informatica se 
+calcula a base de sus calificaciones de cuatro aspectos de
+su rendimiento academico: participacion, primer examen,
+segundo examen, examen final. Sabiendo que las calificaciones 
+anteriores entran a la calificacion final con ponderaciones de 10%,
+25%,25% y 40%, Hacer un programa que calcule e imprima la calificacion final del
+estudiante*/
 import java.util.Scanner;
 
 public class Ejercicio4 {
---
+++ Ejercicio5.java
@@ -0,0 +1,24 @@
+/* Hacer un programa que calcule el 
+ * cuadrado de una suma 
+ * (a+b)^2 = a^2 + b^2 + 2ab
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        float a,b,resultado;
+        
+        System.out.println("Digite el primer numero: ");
+        a = entrada.nextFloat();
+        System.out.println("Digite el segundo numero: ");
+        b = entrada.nextFloat();
+
+        resultado = a*a + b*b + (a*b)*2;
+
+        System.out.println("El resultado es: " + resultado);
+        entrada.close();
+    }
+}
