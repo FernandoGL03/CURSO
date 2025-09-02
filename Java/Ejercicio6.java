7cc2c42bf2228a55f598543c2e03a5e928616d4d
Fernando Gallegos <gallegosfernando0043@gmail.com>
2025-09-02 03:00:52 -0600

    Ultimos ejercicios de Operadores

---
+++ Java/Ejercicio6.java
@@ -0,0 +1,29 @@
+package Java;
+/* Construir un programa que dado un numero total de horas
+ * devuelve el numero de semanas, dias y horas equivalentes.
+ * Por ejemplo, dado un total de 1000 horas debe mostra 5 semanas, 6 dias y 16 horas.
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int Htotales, semanas,dias,horas;
+
+        System.out.println("Digite el numero totales de horas: ");
+        Htotales = entrada.nextInt();
+
+        semanas = Htotales / 168;
+        dias = Htotales%168 / 24;
+        horas = Htotales%24;
+
+        System.out.println("El quivalente es:");
+        System.out.println("Semanas: " + semanas);
+        System.out.println("Dias: " + dias);
+        System.out.println("Horas: " + horas);
+
+        entrada.close();
+    }
+}
---
+++ Java/Ejercicio7.java
@@ -0,0 +1,31 @@
+package Java;
+
+import java.util.Scanner;
+
+/* Construir uun programa que calcule y muestre por pantalla 
+ * las raices de la ecuacion de segundo grado de coeficientes 
+ * reales
+ */
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        double a,b,c,raiz1,raiz2;
+
+        System.out.println("Digite el numero de a: ");
+        a = entrada.nextFloat();
+        System.out.println("Digite el numero de b: ");
+        b = entrada.nextFloat();
+        System.out.println("Digite el numero de c: ");
+        c = entrada.nextFloat();
+
+        raiz1 = -b + Math.sqrt((b*b) - (a*c*4)) / 2*a;
+        raiz2 = -b - Math.sqrt((b*b) - (a*c*4)) / 2*a;
+
+        System.out.println("Las raices son:");
+        System.out.println("Raiz 1: " + raiz1);
+        System.out.println("Raiz 2: " + raiz2);
+
+        entrada.close();
+    }
+}
