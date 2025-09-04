881a19ad5c20196dbcf5b743aafab1f6f8bcb4ee
Fernando Gallegos <gallegofernando0043@gmail.com>
2025-09-03 23:06:36 -0600

    Dia 4

---
+++ Java/Operadores/Incremento_Decremento.java
@@ -0,0 +1,22 @@
+package Java.Operadores;
+
+public class Incremento_Decremento {
+    public static void main(String[] args) {
+        /* El incremento es cuando a un valor en memoria le aumentamos en 1 a ese valor
+         * y el decremento es cuando le restas 1 a ese valor
+         */
+        int valor1 = 3;
+        int valor2 = 2;
+        int suma;
+
+        //Aqui le incrementamos el valor en 1, osea pasa de ser 3 a 4 y en valor2 paso de 2 a 3
+        valor1 ++; 
+        valor2 ++;
+
+        suma = valor1 + valor2;
+        System.out.println("La suma con incremento es: " + suma);
+        
+        suma --;//Aqui a suma le restamos 1 con el operador de decremento
+        System.out.println("La resta es: " + suma);
+    }
+}
---
+++ Java/Operadores/Operadores_de_Asignacion.java
@@ -0,0 +1,12 @@
+package Java.Operadores;
+
+public class Operadores_de_Asignacion {
+    public static void main(String[] args) {
+        
+        int dato = 5;
+
+    //dato = dato + 5 es lo mismo que lo de abajo por eso se le llama de asignacion
+        dato += 5; 
+        System.out.println("El resultado es: " + dato);
+    }
+}
