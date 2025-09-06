f0886691ed0e64ce391f5a36e35f7a103e41ceb7
Fernando Gallegos <gallegofernando0043@gmail.com>
2025-09-05 22:57:39 -0600

    Dia 6.2

--- Java/2.Entrada y Salida de datos/I_O_de_datos_por_consola.java
+++ Java/2.Entrada y Salida de datos/I_O_de_datos_por_consola.java
@@ -9,18 +9,23 @@ public class I_O_de_datos_por_consola {
         int numero;
         float decimal;
         String cadena;
+        char caracter;
 
         System.out.print("Digite un numero: ");
         numero = entrada.nextInt();//Aqui indicamos que tipo de dato vamos a ingrsar en este caso de tipo int
         System.out.println("El numero es: " + numero);
 
-        System.out.print("Digite un numero: ");
+        System.out.print("Digite un numero decimal: ");
         decimal = entrada.nextFloat();//Aqui un nummero Flotante
         System.out.println("El numero es: " + decimal);
 
         System.out.print("Escriba una cadena: ");
         cadena = entrada.next();//Aqui se guardar una cadena hasta que encuentre un espacio
         System.out.println("La cadena es: " + cadena);
+
+        System.out.print("Escriba una letra: ");
+        caracter = entrada.next().charAt(0);//Aqui se guardar una Letra
+        System.out.println("La cadena es: " + caracter);
 
         entrada.close();
     }
