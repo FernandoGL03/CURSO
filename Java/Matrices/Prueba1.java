b479d7a71e19417e5b9087195bacc22aecf73174
Fernando <gallegosfernando0043@gmail.com>
2025-10-23 01:21:22 -0600

    para matrices

---
+++ Java/Matrices/Prueba1.java
@@ -0,0 +1,8 @@
+package Java.Matrices;
+
+public class Prueba1 {
+    public static void main(String[] args) {
+        int [][] Numeros = {{1,2},{4,5}};
+        System.out.println(Numeros.length);
+    }
+}
