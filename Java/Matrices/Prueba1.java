40d7ae4d7a0ee5acc055614588a3d6a5502bf31b
Fernando <gallegosfernando0043@gmail.com>
2025-10-24 01:50:41 -0600

    1

--- Java/Matrices/Prueba1.java
+++ Java/Matrices/Prueba1.java
@@ -1,8 +1,19 @@
 package Java.Matrices;
 
+import javax.swing.JOptionPane;
+
 public class Prueba1 {
     public static void main(String[] args) {
-        int [][] Numeros = {{1,2},{4,5}};
-        System.out.println(Numeros.length);
+        //int [][] Numeros = {{10,66},{1,8}};
+
+        int [][] Numeros = new int[2][2];
+        
+        Numeros[0][0] = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese un numero: "));
+        Numeros[0][1] = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese un numero: "));
+        Numeros[1][0] = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese un numero: "));
+        Numeros[1][1] = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese un numero: "));
+
+        JOptionPane.showMessageDialog(null, Numeros[0][0]);
+
     }
 }
